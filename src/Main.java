376e22b880e561ef84462d653d86bcc649154af9
Jon-Bryce <brycejonathan3@gmail.com>
2019-10-30 17:07:15 -0400

    Added Tick function to Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,38 @@
+//import java.net.URL;
+//import java.util.ResourceBundle;
 
-public class Main {
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.animation.TimelineBuilder;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.util.Duration;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+@SuppressWarnings("deprecation")
+public class Main
+{
 
+	public static void main(String[] args)
+	{
+		Timeline tick = TimelineBuilder.create()
+				.keyFrames
+				(
+					new KeyFrame
+					(
+						//How often the tick updates in milliseconds
+						new Duration(10),
+						new EventHandler<ActionEvent>()
+						{
+							public void handle(ActionEvent event)
+							{
+								//Put code to run every tick in here.
+							}
+						}
+					)
+				)
+				.cycleCount(Timeline.INDEFINITE)
+				.build();
+			//Starts the Timeline Tick
+			tick.play();
 	}
-
 }
